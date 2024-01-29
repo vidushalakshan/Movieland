fbc36ed0f90f3f9522d219452a5a609dc7a1db23
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-01-29 19:11:58 +0530

    aded api call

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ import "./index.css";
 const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
 
   return (
     <div className="app">
